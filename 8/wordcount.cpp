bffa62122281cd7c7b44dd1fd5b1dc18dde8e492
jtbright <jtbright@alaska.edu>
2015-12-10 14:04:06 -0900

    added error checking

--- 8/wordcount.cpp
+++ 8/wordcount.cpp
@@ -1,41 +1,66 @@
-#include <string>
-using std::string;
-using std::size_t;
+// wordcount.cpp
+// Jason Warta and Jason Bright
+// 10 Dec 2015
+//
+// For CS 311 Fall 2015
+// Header for template treesort and associated structures and functions
+// Used in Assignment 8
+
 #include <map>
 using std::map;
 #include <fstream>
 using std::ifstream;
+using std::string;
+using std::size_t;
 #include <iostream>
 using std::cout;
 using std::cin;
 using std::endl;
 	
+/*
+Word count takes a file name as input, and outputs a delimited list of the words found within the file and the number of times it occurs.
+If an error is detected during the file read, the count will be aborted and the program will exit.  
+This program is designed for plain text files.  While it will "do it's best" for other files results are not guaranteed.
+Main will not through an error, 
 
+*/
 int main(){
-	string fname;
+//Get the file name.  
+	string fname;  //file name
 	cout << "Enter a filename: ";
 	getline(cin,fname);
-
+//Open the file
 	ifstream ifs(fname);
+	if(ifs.bad()) {
+		cout << "Error opening file" << endl;
+	}
 	string input;
 	map<string, size_t> count;
 
 	if(ifs.is_open()){
 		while(ifs.good()){
-			ifs >> input;
-			if(count.find(input) == count.end()) count[input] = 1;
-			else count[input]++;
+			try {  //Only known time this throws is if the user has input a directory name on windows
+				ifs >> input;
+			}
+			catch(...){  //This is a controlled exit, not a crash.
+				cout << "Error in file read occurred, exiting" << endl;
+				cout << "Did you try to open a folder?" << endl;
+				ifs.close();
+				return 1;
+			}				
+			if(count.find(input) == count.end()) count[input] = 1; //Word not found in the mapp, add to map
+			else count[input]++; //Word found, increment count for that word.
 
 			if(ifs.eof()) break;
 		}
-
+//Print the listing
 		for(auto i = count.begin(); i != count.end(); i++){
 			cout << i->first << " " << i->second << endl;
 		}
+		ifs.close();
 	}
 	else {
 		cout << "Could not open file." << endl;
 	}
-
 	return 0;
 }
